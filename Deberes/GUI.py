b1cd288dc9dff7197b4999ac441e5aeaa3eb3097
Tony <josantoniosc04@gmail.com>
2025-05-05 13:40:07 -0600

    Implementacion del boton Agregar

--- Deberes/GUI.py
+++ Deberes/GUI.py
@@ -1,11 +1,25 @@
 import Funciones
 import FreeSimpleGUI as sg
 
-label = sg.Text("Escribe un deber: ")
-input = sg.InputText(tooltip="Ingresa un deber: ")
-button = sg.Button("Agregar deber")
 
-window = sg.Window('Mi App de Deberes', layout=[[label], [input, button]])
-window.read()
+label = sg.Text("Escribe un deber: ")
+input = sg.InputText(tooltip="Ingresa un deber: ", key="deber")
+button = sg.Button("Agregar")
+
+window = sg.Window('Mi App de Deberes', layout=[[label], [input, button]],
+                                             font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Agregar":
+            deberes = Funciones.get_deberes()
+            nuevo_deber = values['deber'] + "\n"
+            deberes.append(nuevo_deber)
+            Funciones.write_deberes(deberes)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
 
