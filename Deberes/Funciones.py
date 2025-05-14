8d689d6ac42be3cefa04b3a276a266cb9c199b0d
Tony <josantoniosc04@gmail.com>
2025-05-14 15:42:12 -0600

    Implementacion de la ruta relativa basada en __file__

--- Deberes/Funciones.py
+++ Deberes/Funciones.py
@@ -1,10 +1,16 @@
-def get_deberes(filepath = "Deberes.txt"):
-     with open(filepath, 'r') as file_local:
-          deberes_local = file_local.readlines()
-     return deberes_local
+import os
 
+def get_deberes():
+    carpeta_actual = os.path.dirname(__file__)
+    filepath = os.path.join(carpeta_actual, "Deberes.txt")
 
-def write_deberes(deberes_arg, filepath = "Deberes.txt"):
-     with open(filepath, 'w') as file:
-          file.writelines(deberes_arg)
+    with open(filepath, 'r') as file_local:
+        return file_local.readlines()
+
+def write_deberes(deberes_arg):
+    carpeta_actual = os.path.dirname(__file__)
+    filepath = os.path.join(carpeta_actual, "Deberes.txt")
+
+    with open(filepath, 'w') as file:
+        file.writelines(deberes_arg)
 
